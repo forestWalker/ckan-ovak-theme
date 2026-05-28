60f5c5cc1d5bc9d06a421163b4c6b983cef524d9
forestWalker <a.hoperay@gmail.com>
2026-05-28 07:18:49 +0200

    fix: table display, dynamic handling, hardcoded charts

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ckanext-ovak-theme",
-    version="0.0.1",
+    version="0.1.0",
     description="OVAK custom CKAN theme",
     packages=['ckanext.ovak_theme'],
     package_dir={'ckanext.ovak_theme': 'ckanext/ovak_theme', '': '.'},
